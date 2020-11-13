27b4cfe5e4d108a89d10743e9c5149e36ab47645
R-Mat31 <73731479+R-Mat31@users.noreply.github.com>
2020-11-13 10:33:01 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+ apiKey : "AIzaSyBFG3L-uiFVk6Njv7kjqx7LELnCv_QCvt8",
+ authDomain : "let-s-chat-8d7b1.firebaseapp.com",
+ databaseURL : "https://let-s-chat-8d7b1.firebaseio.com",
+ projectId : "let-s-chat-8d7b1",
+ storageBucket : "let-s-chat-8d7b1.appspot.com",
+ messagingSenderId : "356240475932",
+ appId : "1:356240475932:web:2e0b205cb7ecf5a1701cd9"
+};
+firebase.initializeApp(firebaseConfig);
