3d7fd726b4ef947cf75e9436e0a2d27669cec704
R-Mat31 <73731479+R-Mat31@users.noreply.github.com>
2020-11-24 15:18:05 +0530

    Add files via upload

--- chat_room.js
+++ chat_room.js
@@ -8,3 +8,35 @@ var firebaseConfig = {
  appId : "1:356240475932:web:2e0b205cb7ecf5a1701cd9"
 };
 firebase.initializeApp(firebaseConfig);
+var userName = localStorage.getItem("name");
+document.getElementById("User_Name").innerHTML = "Welcome " + userName + "!";
+function add_room(){
+ roomName = document.getElementById("roomname").value;
+ localStorage.setItem("room_name", roomName);
+ firebase.database().ref("/").child(roomName).update({
+  Creator : userName   
+ });
+ window.location = "chat_page.html";
+}
+function getData(){
+ firebase.database().ref("/").on('value', function(snapshot){
+  document.getElementById("output").innerHTML = "";
+  snapshot.forEach(function(childSnapshot){
+   childKey = childSnapshot.key;
+   Room_names = childKey;
+   roomDiv = "<div class = 'room_name' id = " + Room_names + " onclick = 'redirectToRoomName(this.id)'>" + Room_names + "</div> <hr>";
+   document.getElementById("rooms").innerHTML += roomDiv;
+  });
+ });
+}
+getData();
+function redirectToRoomName(name){
+ console.log(name);
+ localStorage.setItem("roomNAME", name);
+ window.location = "chat_page.html";
+}
+function logout(){
+ localStorage.removeItem("name");
+ localStorage.removeItem("room_name");
+ window.location = "index.html"; 
+} 
